20cc4bf04ced91135ef7ca67179f084a8e921624
Rouven <seifertrouven@gmail.com>
2021-05-05 19:08:43 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -nfs /home/$USER/dotfiles/.vimrc /home/$USER/.vimrc
+ln -nfs /home/$USER/dotfiles/.gitconfig /home/$USER/.gitconfig
+ln -nfs /home/$USER/dotfiles/.Xresources /home/$USER/.Xresources
+
+ln -nfs /home/$USER/dotfiles/.config/i3/config /home/$USER/.config/i3/config
+ln -nfs /home/$USER/dotfiles/.config/i3status/config /home/$USER/.config/i3status/config
