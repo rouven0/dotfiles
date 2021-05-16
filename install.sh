34b0fb03c0e3fe669f6511e602b585411c4ca8f2
Rouven <seifertrouven@gmail.com>
2021-05-16 17:14:08 +0200

    added mkdir in the install

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ ln -nfs /home/$USER/dotfiles/.vimrc /home/$USER/.vimrc
 ln -nfs /home/$USER/dotfiles/.gitconfig /home/$USER/.gitconfig
 ln -nfs /home/$USER/dotfiles/.Xresources /home/$USER/.Xresources
 
+mkdir /home/$USER/.config/i3
+mkdir /home/$USER/.config/i3status
 ln -nfs /home/$USER/dotfiles/.config/i3/config /home/$USER/.config/i3/config
 ln -nfs /home/$USER/dotfiles/.config/i3status/config /home/$USER/.config/i3status/config
