7c8bc4664c6eaa05cc6070c81328e56fb2039a1f
Rouven <seifertrouven@gmail.com>
2021-06-27 15:19:41 +0200

    removed zsh

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 ln -nfs /home/$USER/dotfiles/.vimrc /home/$USER/.vimrc
 ln -nfs /home/$USER/dotfiles/.gitconfig /home/$USER/.gitconfig
-ln -nfs /home/$USER/dotfiles/.zshrc /home/$USER/.zshrc
 
 mkdir /home/$USER/.config/i3
 mkdir /home/$USER/.config/i3status
+mkdir /home/$USER/.config/dunst
 ln -nfs /home/$USER/dotfiles/.config/i3/config /home/$USER/.config/i3/config
 ln -nfs /home/$USER/dotfiles/.config/i3status/config /home/$USER/.config/i3status/config
+ln -nfs /home/$USER/dotfiles/.config/dunst/dunstrc /home/$USER/.config/dunst/dunstrc
