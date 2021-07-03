e142d9b835fca8f40fd00624507568d7c0892a33
Rouven <seifertrouven@gmail.com>
2021-07-03 13:20:21 +0200

    added fontconfig

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ ln -nfs /home/$USER/dotfiles/.gitconfig /home/$USER/.gitconfig
 mkdir /home/$USER/.config/i3
 mkdir /home/$USER/.config/i3status
 mkdir /home/$USER/.config/dunst
+mkdir /home/$USER/.config/fontconfig
 ln -nfs /home/$USER/dotfiles/.config/i3/config /home/$USER/.config/i3/config
 ln -nfs /home/$USER/dotfiles/.config/i3status/config /home/$USER/.config/i3status/config
 ln -nfs /home/$USER/dotfiles/.config/dunst/dunstrc /home/$USER/.config/dunst/dunstrc
+ln -nfs /home/$USER/dotfiles/.config/fontconfig/fonts.conf /home/$USER/.config/fontconfig/fonts.conf
