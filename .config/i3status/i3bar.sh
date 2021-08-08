fbc1a82cd07b61fd40727b7c4ebf096eae7c9170
Rouven <seifertrouven@gmail.com>
2021-08-08 15:17:19 +0200

    removed useless information in i3bar

--- .config/i3status/i3bar.sh
+++ .config/i3status/i3bar.sh
@@ -4,13 +4,13 @@
 i3status | while :
 do
         read line
-        current_player=$(playerctl -l | head -1)
+        current_player=$(playerctl -l | head -1 | cut -d "." -f1)
         player_status=$(playerctl status)
 
         if [ "$current_player" = "" ]; then
                 echo $line || exit 1
         else
-                echo "$player_status $current_player $status_symbol| $line" || exit 1
+                echo "$player_status $current_player| $line" || exit 1
         fi;
 
 done
