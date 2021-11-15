18e23f8e0f0437b6ade049f0de73c07a732a906f
Rouven <seifertrouven@gmail.com>
2021-11-15 21:40:55 +0100

    just some other stuff

--- .config/i3status/i3bar.sh
+++ .config/i3status/i3bar.sh
@@ -9,9 +9,9 @@ do
         current_layout=$(setxkbmap -query | grep variant | cut -d " " -f5)
 
         if [ "$current_player" = "" ]; then
-                echo "$line | $current_layout " || exit 1
+                echo "$line | $current_layout" || exit 1
         else
-                echo "$player_status $current_player | $line | $current_layout " || exit 1
+                echo "$player_status $current_player | $line | $current_layout" || exit 1
         fi;
 
 done
