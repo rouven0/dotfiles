1b2b06d0026d6ee964acb13dfc918e97a2f14376
Rouven <seifertrouven@gmail.com>
2021-12-12 15:59:03 +0100

    enhanced keyboard layout info

--- .config/i3status/i3bar.sh
+++ .config/i3status/i3bar.sh
@@ -6,12 +6,13 @@ do
         read line
         current_player=$(playerctl -l | head -1 | cut -d "." -f1)
         player_status=$(playerctl status)
-        current_layout=$(setxkbmap -query | grep variant | cut -d " " -f5)
+        current_layout=$(setxkbmap -query | grep layout | cut -d " " -f6)
+        current_variant=$(setxkbmap -query | grep variant | cut -d " " -f5)
 
         if [ "$current_player" = "" ]; then
-                echo "$line | $current_layout" || exit 1
+                echo "$line | $current_layout $current_variant" || exit 1
         else
-                echo "$player_status $current_player | $line | $current_layout" || exit 1
+                echo "$player_status $current_player | $line | $current_layout $current_variant" || exit 1
         fi;
 
 done
